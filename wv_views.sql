87fa05f8704cad4b31b4f9660253983cb5d347a9
Fod9 <dernoncourt.tom@gmail.com>
2025-03-21 15:48:09 +0100

    switch alter table to index file

--- wv_views.sql
+++ wv_views.sql
@@ -1,12 +1,4 @@
 -- Modification des colonnes de type TEXT en VARCHAR
-ALTER TABLE parties ALTER COLUMN title_party VARCHAR(255);
-ALTER TABLE roles ALTER COLUMN description_role VARCHAR(255);
-ALTER TABLE players ALTER COLUMN pseudo VARCHAR(255);
-ALTER TABLE players_in_parties ALTER COLUMN is_alive VARCHAR(50);
-ALTER TABLE players_play ALTER COLUMN origin_position_col VARCHAR(50);
-ALTER TABLE players_play ALTER COLUMN origin_position_row VARCHAR(50);
-ALTER TABLE players_play ALTER COLUMN target_position_col VARCHAR(50);
-ALTER TABLE players_play ALTER COLUMN target_position_row VARCHAR(50);
 
 DROP VIEW ALL_PLAYERS_ELAPSED_GAME
 
