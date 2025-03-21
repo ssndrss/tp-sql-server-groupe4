1076693a65b83c55338d6a1b188415228c585abd
Sandra Sini <sandra@MacBook-Pro-de-Sandra.local>
2025-03-21 17:00:32 +0100

    deletions in wv_index.sql

--- wv_funcs.sql
+++ wv_funcs.sql
@@ -35,4 +35,4 @@ BEGIN
     END;
 
     RETURN;
-END;
+END; 
--- wv_index.sql
+++ wv_index.sql
@@ -7,22 +7,4 @@ ALTER TABLE players_play ALTER COLUMN origin_position_row VARCHAR(50);
 ALTER TABLE players_play ALTER COLUMN target_position_col VARCHAR(50);
 ALTER TABLE players_play ALTER COLUMN target_position_row VARCHAR(50);
 
-CREATE TABLE possible_positions (
-    row INT,
-    col INT
-);
-
-DECLARE @i INT = 0, @j INT;
-
-WHILE @i < 10
-BEGIN
-    SET @j = 0;
-    WHILE @j < 10
-    BEGIN
-        INSERT INTO possible_positions VALUES (@i, @j);
-        SET @j = @j + 1;
-    END
-    SET @i = @i + 1;
-END;
-
 
